676a505d7b5f385a96e4000b255271d11a0ebf9c
Sam Wellander <sam@simplefractal.com>
2020-06-28 12:40:27 -0400

    Progress

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 from sms import send
 # from dotenv import load_dotenv
@@ -18,6 +18,7 @@ from loading_spinner import Spinner
 target = "nord"
 url = os.getenv('MONITORED_URL')
 url = "https://seattle.craigslist.org/search/sss?query=nord&sort=rel&srchType=T&bundleDuplicates=1&searchNearby=2&nearbyArea=217&nearbyArea=233&nearbyArea=350&nearbyArea=322&nearbyArea=94&nearbyArea=324&nearbyArea=654&nearbyArea=655&nearbyArea=466&nearbyArea=321&nearbyArea=9&nearbyArea=368&nearbyArea=459&nearbyArea=232&nearbyArea=461&nearbyArea=95&nearbyArea=325&nearbyArea=246"
+# url = "https://seattle.craigslist.org/search/sss?query=nord&sort=rel&srchType=T&postedToday=1&bundleDuplicates=1&searchNearby=2&nearbyArea=217&nearbyArea=233&nearbyArea=350&nearbyArea=322&nearbyArea=94&nearbyArea=324&nearbyArea=654&nearbyArea=655&nearbyArea=466&nearbyArea=321&nearbyArea=9&nearbyArea=368&nearbyArea=459&nearbyArea=232&nearbyArea=461&nearbyArea=95&nearbyArea=325&nearbyArea=246"
 list_selector = os.getenv('LIST_SELECTOR')
 item_selector = os.getenv('ITEM_SELECTOR')
 success_msg = os.getenv('SUCCESS_MSG', f'Success! Your target was found at {url}')
@@ -27,17 +28,13 @@ def item_is_in_list(list_items=[]):
     return any(target.lower() in item.text.lower() for item in list_items)
 
 
-def get_chrome_driver():
-    chrome_bin = os.getenv('GOOGLE_CHROME_SHIM')
-    executable_path = str(os.getenv('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver'))
-    chrome_options = Options()
-    chrome_options.binary_location = chrome_bin
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--remote-debugging-port=9222')
-    return webdriver.Chrome(
-        executable_path=executable_path, options=chrome_options
+def get_gecko_driver():
+    firefox_bin = os.getenv('GOOGLE_CHROME_SHIM', '/Applications/Firefox.app')
+    executable_path = str(os.getenv('CHROMEDRIVER_PATH', '/usr/local/bin/geckodriver'))
+    options = Options()
+    options.add_argument("--headless")
+    return webdriver.Firefox(
+        executable_path=executable_path, options=options
     )
 
 
@@ -46,7 +43,7 @@ def run_scan():
     print(f'at url: {url}')
     print('====================================================')
 
-    driver = get_chrome_driver()
+    driver = get_gecko_driver()
     driver.implicitly_wait(3000)
     driver.get(url)
 
@@ -54,12 +51,14 @@ def run_scan():
 
     # Wait for page to load
     print('Waiting for page to load')
-    with Spiner():
-        time.sleep(10)
+    # with Spinner():
+    #     time.sleep(10)
     print('Page loaded')
 
     print('Scanning')
     first_page = BeautifulSoup(driver.page_source, features="html.parser")
+
+    keyboards = first_page.findAll('li', {'class': 'result-row'})
     import pdb; pdb.set_trace()
 
     # If searching for item within a list
